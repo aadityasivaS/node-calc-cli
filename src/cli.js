34fb8eb454c6daea922bb44c11b71ec54c950a15
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2021-01-29 11:31:16 +0530

    minor change

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 let args = process.argv.slice(2);
-if (args === null) {
+if (args[0] === null) {
     console.log('😅 heres some help do like this n-c-c 1+1');
 } else {
     console.log(eval(args[0]));
